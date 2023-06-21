ebb9396172f3988d0ce80166c205a9f98c2b12b8
Andrew Chernoff <whitedrew538@gmail.com>
2023-06-21 17:33:23 +0500

    vercel.json src root changed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 const app = express()
-const port =process.env.PORT || 3010
+const port = process.env.PORT || 3010
 
 app.get('/', (req: Request, res: Response) => {
   let helloMessage = 'Hello world!!!!'
