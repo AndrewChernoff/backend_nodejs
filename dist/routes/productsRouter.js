82d57121bef0845316e158dd3a972419db4e089f
Andrew Chernoff <whitedrew538@gmail.com>
2023-07-04 22:27:26 +0500

    validation

--- dist/routes/productsRouter.js
+++ dist/routes/productsRouter.js
@@ -3,55 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.productsRouter = void 0;
 const productsRepositories_1 = require("./../repositories/productsRepositories");
 const express_1 = require("express");
+const express_validator_1 = require("express-validator");
 /* const products = [{id: 1, title: 'orange'}, {id: 2, title: 'tomato'}]*/
+const validationErrorMiddleware = (req, res, next) => {
+    const result = (0, express_validator_1.validationResult)(req);
+    if (!result.isEmpty()) {
+        return res.status(400).send({ errors: result.array() });
+    }
+    next();
+};
 exports.productsRouter = (0, express_1.Router)();
-exports.productsRouter.get('/', function (req, res) {
+const titleValidation = (0, express_validator_1.body)('title')
+    .isLength({ min: 3, max: 10 })
+    .withMessage('Should be more than 3 and less than 10 symbols');
+exports.productsRouter.get("/", function (req, res) {
     const param = req.query.title;
     if (param) {
         const product = productsRepositories_1.productsRepositories.getProduct(param === null || param === void 0 ? void 0 : param.toString());
         res.send(product);
     }
     res.send(productsRepositories_1.productsRepositories.getProduct(null));
-    /* if(param) {
-      res.send(products.find(el => el.title.indexOf(param.toString()) > -1))
-    }
-    res.send(products) */
 });
-exports.productsRouter.delete('/:id', function (req, res) {
+exports.productsRouter.delete("/:id", function (req, res) {
     const param = req.params.id;
     const isDeleted = productsRepositories_1.productsRepositories.removeProduct(+param);
     isDeleted ? res.send(204) : res.send(404);
-    /* for (let i = 0; i < products.length; i++) {
-      if (products[i].id === +param) {
-        products.splice(i,1)
-        res.send(204)
-        return
-      }
-    }
-    res.send(404) */
 });
-exports.productsRouter.post('/', function (req, res) {
-    /* const newProduct = {
-        id: new Date().getSeconds(),
-        title: req.body.title
-      }
-      products.push(newProduct)
-      res.send(204); */
+exports.productsRouter.post("/", titleValidation, validationErrorMiddleware, function (req, res) {
     const product = productsRepositories_1.productsRepositories.createProduct(req.body.title.toString());
     if (product) {
-        res.send(204);
+        res.send(product);
     }
 });
-exports.productsRouter.put('/:id', function (req, res) {
+exports.productsRouter.put("/:id", titleValidation, validationErrorMiddleware, function (req, res) {
     const product = productsRepositories_1.productsRepositories.updateProduct(+req.params.id, req.body.title);
     product ? res.send(product) : res.send(404);
-    /* const product = products.find(el => el.id === +req.params.id)
-
-      if(product) {
-        product.title = req.body.title
-        res.send(product)
-        return
-      }
-    
-      res.send(404); */
 });
--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,67 +1,62 @@
-import { productsRepositories } from './../repositories/productsRepositories';
-import { Router } from "express";
+import { productsRepositories } from "./../repositories/productsRepositories";
+import { NextFunction, Router } from "express";
+import { body, matchedData, query, validationResult } from 'express-validator';
+import express, { Request, Response } from 'express';
 
 /* const products = [{id: 1, title: 'orange'}, {id: 2, title: 'tomato'}]*/
 
-export const productsRouter = Router()
-
-productsRouter.get('/', function(req, res) {
-    const param = req.query.title
-
-    if(param) {
-      const product = productsRepositories.getProduct(param?.toString() as string)
-      res.send(product)
+const validationErrorMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.status(400).send({ errors: result.array() });
     }
+    next()
 
-    res.send(productsRepositories.getProduct(null))
+}
 
-    /* if(param) {
-      res.send(products.find(el => el.title.indexOf(param.toString()) > -1))
-    } 
-    res.send(products) */
-  });
+export const productsRouter = Router();
 
-productsRouter.delete('/:id', function(req, res) {
-    const param = req.params.id
+const titleValidation = body('title')
+.isLength({min: 3, max: 10})
+.withMessage('Should be more than 3 and less than 10 symbols')
 
-    const isDeleted = productsRepositories.removeProduct(+param)
-    isDeleted ? res.send(204) : res.send(404) 
-    /* for (let i = 0; i < products.length; i++) {
-      if (products[i].id === +param) {
-        products.splice(i,1)
-        res.send(204)
-        return
-      }
-    }
-    res.send(404) */
-  });
+productsRouter.get("/", function (req, res) {
+  const param = req.query.title;
+
+  if (param) {
+    const product = productsRepositories.getProduct(
+      param?.toString() as string
+    );
+    res.send(product);
+  }
+
+  res.send(productsRepositories.getProduct(null));
+});
+
+productsRouter.delete("/:id", function (req, res) {
+  const param = req.params.id;
+
+  const isDeleted = productsRepositories.removeProduct(+param);
+  isDeleted ? res.send(204) : res.send(404);
+});
+
+
+productsRouter.post("/", titleValidation, validationErrorMiddleware, function (req, res) {
   
-productsRouter.post('/', function(req, res) {
-    /* const newProduct = {
-        id: new Date().getSeconds(),
-        title: req.body.title
-      }
-      products.push(newProduct)
-      res.send(204); */
-      const product = productsRepositories.createProduct(req.body.title.toString() as string)
-      if (product) {
-        res.send(204)
-      }
-    });
 
-    productsRouter.put('/:id', function(req, res) {
+  const product = productsRepositories.createProduct(
+    req.body.title.toString() as string
+  );
+  if (product) {
+    res.send(product);
+  }
+});
 
-      const product = productsRepositories.updateProduct(+req.params.id, req.body.title )
-      
-      product ? res.send(product) : res.send(404)
-      /* const product = products.find(el => el.id === +req.params.id)
-  
-        if(product) {
-          product.title = req.body.title
-          res.send(product)
-          return
-        }
-      
-        res.send(404); */    
-    });
+productsRouter.put("/:id", titleValidation, validationErrorMiddleware, function (req, res) {
+  const product = productsRepositories.updateProduct(
+    +req.params.id,
+    req.body.title
+  );
 
+  product ? res.send(product) : res.send(404);
+});
